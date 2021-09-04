d19961003ac89d48188d482b6ed82a4d46e33fed
keva damar galih <kevadamarg@gmail.com>
2021-09-04 22:32:58 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./src/routes');
 const app = express();
-const http = require('http').createServer(app);
 
 app.use(cors());
 
@@ -14,4 +13,4 @@ const PORT = process.env.PORT_ENV || 5000;
 
 app.use('/api/v1', router);
 
-http.listen(PORT, () => console.log(`server running on port : ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port : ${PORT}`));
